d75cf72014dfd22c1b80ed9517996bf334a5a8ff
rahulhasan31 <rahulhasan666@gmail.com>
2022-10-25 19:40:03 +0600

    apiReady

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(cors())
 const port= process.env.PORT||5000
 
 const categories= require('./Data/category.json')
+const courses=require('./Data/CoursesDetails.json')
 
 app.get('/', (req, res)=>{
     res.send('server running sei')
@@ -15,8 +16,23 @@ app.get('/categories', (req, res)=>{
    res.send(categories);
 })
 
+app.get('/category/:id', (req, res)=>{
+    const id=req.params.id
+    const categoryId=courses.filter(c=>c.course_id===id)
+    res.send(categoryId)
+})
 
 
+app.get('/course/:id', (req, res)=>{
+    const id= req.params.id
+    const course=courses.find(c=>c._id === id)
+    res.send(course)
+})
+
+
+app.get('/course', (req, res)=>{
+    res.send(courses)
+})
 
 
 app.listen(port, ()=>{
