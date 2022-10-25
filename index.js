b33f5282ae583db88e5b086cf4063f3fc9172020
rahulhasan31 <rahulhasan666@gmail.com>
2022-10-25 17:13:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express= require('express')
+const app= express()
+const cors= require('cors')
+
+app.use(cors())
+const port= process.env.PORT||5000
+
+const categories= require('./Data/category.json')
+
+app.get('/', (req, res)=>{
+    res.send('server running sei')
+})
+
+app.get('/categories', (req, res)=>{
+   res.send(categories)
+})
+
+
+
+
+
+app.listen(port, ()=>{
+  console.log('server coltese ',  port);
+})
